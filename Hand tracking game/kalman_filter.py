ed193c6a88b65c0aa7663e2e649dfa48c781ef2d
Tomasz <kurek.tom@gmail.com>
2019-05-23 16:19:49 +0200

    Add kalman filter

---
+++ Hand tracking game/kalman_filter.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+
+class KalmanFilter:
+    kf = cv2.KalmanFilter(4, 2)
+    kf.measurementMatrix = np.array([[1, 0, 0, 0], [0, 1, 0, 0]], np.float32)
+    kf.transitionMatrix = np.array([[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32)
+
+    def estimate(self, coordX, coordY):
+        measured = np.array([[np.float32(coordX)], [np.float32(coordY)]])
+        self.kf.correct(measured)
+        predicted = self.kf.predict()
+        return predicted
